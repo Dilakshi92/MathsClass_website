c2135df0f64718ed792b43e9673997586a12289a
Dilakshi92 <laxdj185@gmail.com>
2026-01-13 13:55:27 +0530

    Initial commit

---
+++ logic.py
@@ -0,0 +1,9 @@
+# main.py
+# This file is linked via PyScript in the HTML and can be used for future interactivity.
+
+from pyscript import display
+
+def greet():
+    display("Hello, welcome to Epson Maths Tuition site!")
+
+
